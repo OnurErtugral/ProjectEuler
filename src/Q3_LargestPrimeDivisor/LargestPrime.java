4aaefb42401ace023c00afd28074a3f751249f53
OnurErtugral <onur-ertugral@hotmail.com>
2017-07-24 20:35:28 +0300

    Improvement

--- src/Q3_LargestPrimeDivisor/LargestPrime.java
+++ src/Q3_LargestPrimeDivisor/LargestPrime.java
@@ -1,31 +1,38 @@
 package Q3_LargestPrimeDivisor;
 
 
+import java.util.ArrayList;
+
 /**
  * Created by onur on 14.07.2017.
  */
 public class LargestPrime {
     public static void main(String[] args) {
-        long x = 10098983456L;
-        int count = 1;
-        boolean isPrime = true;
+        long x = 1000000000L;
+        long max =-1;
+        long target = 600851475143L;
 
-        for (long i = 2; i < x/2; i++) {
-            if (x % i == 0) {
-                System.out.printf("%s ", i);
-                for (int j = 2; j < i/2; j++) {
-                    if (i % j == 0) {
-                        isPrime = false;
-                        break;
-                    }
+        boolean[] isPrime = new boolean[(int)(x+1)];
+        for (int i = 2; i <= x; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int factor = 2; factor*factor <= x; factor++) {
+
+            if (isPrime[factor]) {
+                for (int j = factor; factor*j <= x; j++) {
+                    isPrime[factor*j] = false;
                 }
-                if (isPrime) {
-                    System.out.printf("%d. Prime", count);
-                    ++count;
-                }
-            System.out.println();
             }
         }
-        System.out.println("Done!");
+
+        for (long i = x / 2; i >= 3 ; i--) {
+            if (isPrime[(int) i] && target % i == 0) {
+                max = i;
+                break;
+            }
+        }
+        System.out.println("Max prime divisor: " + max);
     }
+
 }
