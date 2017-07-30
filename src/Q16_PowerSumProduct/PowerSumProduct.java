ffd8e6683c0020daa7d5454316c9e405e3d78021
OnurErtugral <onur-ertugral@hotmail.com>
2017-07-30 19:31:11 +0300

    Initial commit.

---
+++ src/Q16_PowerSumProduct/PowerSumProduct.java
@@ -0,0 +1,21 @@
+package Q16_PowerSumProduct;
+
+import java.math.BigInteger;
+
+/**
+ * Created by Onur Ertugral on 30.07.2017.
+ */
+public class PowerSumProduct {
+    public static void main(String[] args) {
+        BigInteger product = new BigInteger(String.valueOf(1));
+        long sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            product = product.multiply(BigInteger.valueOf(2));
+        }
+        String temp = product.toString();
+        for (int i = 0; i < temp.length(); i++) {
+            sum += Integer.valueOf(Character.toString(temp.charAt(i)));
+        }
+        System.out.println(sum);
+    }
+}
