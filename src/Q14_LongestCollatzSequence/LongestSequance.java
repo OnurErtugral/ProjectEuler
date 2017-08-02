e94f62ac2f90d64b48606b23bc0751e7fd2d9bd3
OnurErtugral <onur-ertugral@hotmail.com>
2017-08-02 14:13:48 +0300

    Small edition.

--- src/Q14_LongestCollatzSequence/LongestSequance.java
+++ src/Q14_LongestCollatzSequence/LongestSequance.java
@@ -15,6 +15,7 @@ public class LongestSequance {
             long n = i;
             int sequance = 1;
             System.out.printf("%d: %d ", i, n);
+
             while (n > 1) {
                 if (n % 2 == 0) {
                     sequance++;
